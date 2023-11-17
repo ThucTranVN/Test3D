9fcef748167d445419a20f916c7558e71e9be0d0
Thuc Tran <thuc.ttk@gmail.com>
2023-11-17 20:31:19 +0700

    [add] root motion

---
+++ Assets/MyGame/Scripts/Learn/RootMotionTest.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootMotionTest : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", false);
+        }
+    }
+}
