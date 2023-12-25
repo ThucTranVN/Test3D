21934ab07eb83fd3fa5b5af50540928836293665
Thuc Tran <thuc.ttk@gmail.com>
2023-12-25 21:32:40 +0700

    [add] character aiming

--- Assets/MyGame/Scripts/Project/Gameplay/CharacterAiming.cs
+++ Assets/MyGame/Scripts/Project/Gameplay/CharacterAiming.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class CharacterAiming : MonoBehaviour
 {
+    public Rig aimLayer;
     public float turnSpeed = 15f;
+    public float aimDuration = 0.3f;
     private Camera mainCamera;
 
     // Start is called before the first frame update
@@ -13,6 +16,18 @@ public class CharacterAiming : MonoBehaviour
         mainCamera = Camera.main;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            aimLayer.weight += Time.deltaTime / aimDuration;
+        }
+        else
+        {
+            aimLayer.weight -= Time.deltaTime / aimDuration;
+        }
     }
 
     // Update is called once per frame
